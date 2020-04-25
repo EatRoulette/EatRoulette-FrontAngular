1ba0c752d6bbae857eef3600ba90374921435a41
LAymeric <aymeric.larvet@gmail.com>
2020-04-25 17:47:53 +0200

    initiation of register feature

---
+++ src/app/components/register/register.spec.ts
@@ -0,0 +1,7 @@
+import { Register } from './services/register';
+
+describe('Register', () => {
+  it('should create an instance', () => {
+    expect(new Register()).toBeTruthy();
+  });
+});
---
+++ src/app/components/register/register.ts
@@ -0,0 +1,5 @@
+export class Register { 
+    Password:string;  
+    Email:string;  
+    Login:string;  
+}
