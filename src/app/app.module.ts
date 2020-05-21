dcfc3f84e6c2bd7acfca4e639fc7a6b4276d8ac4
LAymeric <aymeric.larvet@gmail.com>
2020-05-21 15:25:35 +0200

    :sparkles: initialisation of sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NotFoundComponent} from './components/not-found/not-found.component';
 import { StorageServiceModule } from "ngx-webstorage-service";
 import { SituationComponent } from './components/situation/situation.component';
 import { PizzaLoaderComponent } from './components/pizza-loader/pizza-loader.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { PizzaLoaderComponent } from './components/pizza-loader/pizza-loader.com
     DashboardComponent,
     NotFoundComponent,
     SituationComponent,
-    PizzaLoaderComponent
+    PizzaLoaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
