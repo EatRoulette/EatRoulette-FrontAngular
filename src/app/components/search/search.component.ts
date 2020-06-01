ed2cb37f421ea03464714146d6723ca45a5d4763
LAymeric <aymeric.larvet@gmail.com>
2020-06-01 14:45:11 +0200

    :sparkles: search call + display no result if no result + display error message (raf display restaurant when api ready)

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { Restaurant } from "../../data/restaurant";
+import {SearchService} from "../../services/search/search.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-search',
@@ -8,19 +11,51 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class SearchComponent implements OnInit {
   SearchForm: FormGroup;
+  restaurants: Restaurant[];
+  errorMessage: string;
+  submitted: boolean = false;
+  hasResults: boolean = false;
+  searchService: SearchService;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private router: Router, private formBuilder: FormBuilder, searchService: SearchService) {
+    this.searchService = searchService;
+  }
 
   ngOnInit(): void {
     this.SearchForm = this.formBuilder.group({
       name: ['', []],
       city: ['', []],
       postalCode: ['', []],
-    }); // Todo validator => at least one field filled
+    });
   }
 
   onFormSubmit(){
-    // todo
+    this.hasResults = false;
+    this.submitted = true;
+    this.errorMessage = undefined;
+    const searchValues = this.SearchForm.value;
+    if(this.isPresent(searchValues.name) || this.isPresent(searchValues.type) || this.isPresent(searchValues.postalCode)){
+      this.searchService.search(searchValues).subscribe(
+        (response: any) => {
+          this.hasResults = true;
+          this.restaurants = response;
+        },
+        (error: any) => {
+          console.error(error);
+          this.errorMessage = error.error.message ? error.error.message : "Une erreur est survenue";
+        });
+    }else{
+      this.errorMessage = "Veuillez saisir au moins un critère de recherche"
+    }
+  }
+
+  gotToAdd(){
+    this.router.navigate(['add', this.SearchForm.value])
+    // pourra alors être récupéré avec this.route.snapshot.paramMap.get('parameterName');
+  }
+
+  isPresent(value){
+    return value && value !== "";
   }
 
 }
