da1ced7f0c55bc1abaecf76a3d665e2c912ab3d4
Gwen <gwendou95@hotmail.fr>
2020-07-09 15:03:38 +0200

    ✅ Add logout test

--- src/app/components/sidebar/sidebar.component.spec.ts
+++ src/app/components/sidebar/sidebar.component.spec.ts
@@ -1,25 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SidebarComponent } from './sidebar.component';
+import {browser, by, element} from 'protractor';
 
 describe('SidebarComponent', () => {
-  let component: SidebarComponent;
-  let fixture: ComponentFixture<SidebarComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SidebarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    browser.executeScript('window.localStorage.setItem(\'access_token\',\'edd515a0b007d98f3361949c242bdb02ecc4efa696e154d9c4787885e02363b6\');');
+    browser.get('http://localhost:4200/');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Logout test', () => {
+    element(by.id('logout')).click();
+    browser.driver.sleep(1000);
+    const expectedUrl = browser.driver.getCurrentUrl();
+    expect(expectedUrl).toEqual('http://localhost:4200/login');
+    expect(element(by.id('success')).isPresent()).toBe(true);
+    const value = browser.executeScript('return window.localStorage.getItem(\'access_token\');');
+    expect(value).toBe('');
   });
+
 });
