c6fb2a0f001c6360e9d92397a637b73b4befbde8
Gwen <gwendou95@hotmail.fr>
2020-07-09 14:35:35 +0200

    :white_check_mark: Register test unit

--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -1,25 +1,76 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegisterComponent } from './register.component';
+import {browser, by, element} from 'protractor';
 
 describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    browser.get('http://localhost:4200/register');
+    element(by.id('lname')).sendKeys('UNIT');
+    element(by.id('fname')).sendKeys('Test');
+    element(by.id('email')).sendKeys('test@unit.fr');
+    element(by.id('pwd')).sendKeys('password');
+    element(by.id('address')).sendKeys('3 street Unit Test');
+    element(by.id('town')).sendKeys('PARIS');
+    element(by.id('cp')).sendKeys('75012');
+    element(by.id('phone')).sendKeys('0123456789');
+    element(by.id('cgu')).click();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Register - All fields are empty', () => {
+    element(by.id('lname')).sendKeys('');
+    element(by.id('fname')).sendKeys('');
+    element(by.id('email')).sendKeys('');
+    element(by.id('pwd')).sendKeys('');
+    element(by.id('address')).sendKeys('');
+    element(by.id('town')).sendKeys('');
+    element(by.id('cp')).sendKeys('');
+    element(by.id('phone')).sendKeys('');
+    element(by.id('cgu')).click();
+    element(by.id('btnSubmit')).click();
+    browser.driver.sleep(1000);
+    expect(element(by.id('nameError')).isPresent()).toBe(true);
+    expect(element(by.id('emailError')).isPresent()).toBe(true);
+    expect(element(by.id('pwdError')).isPresent()).toBe(true);
+    expect(element(by.id('addressError')).isPresent()).toBe(true);
+    expect(element(by.id('townError')).isPresent()).toBe(true);
+    expect(element(by.id('cpError')).isPresent()).toBe(true);
+    expect(element(by.id('phoneError')).isPresent()).toBe(true);
+    expect(element(by.id('cguError')).isPresent()).toBe(true);
   });
+
+  it('Register - Invalid email format', () => {
+    element(by.id('email')).sendKeys('badEmail.fr@');
+    element(by.id('btnSubmit')).click();
+    browser.driver.sleep(1000);
+    expect(element(by.id('emailError')).isPresent()).toBe(true);
+  });
+
+  it('Register - Invalid postal code', () => {
+    element(by.id('cp')).sendKeys('Toto');
+    element(by.id('btnSubmit')).click();
+    browser.driver.sleep(1000);
+    expect(element(by.id('cpError')).isPresent()).toBe(true);
+    element(by.id('cp')).sendKeys('74852186');
+    element(by.id('btnSubmit')).click();
+    expect(element(by.id('cpError')).isPresent()).toBe(true);
+  });
+
+  it('Register - Invalid phone number ', () => {
+    element(by.id('phone')).sendKeys('PhoneNumber');
+    element(by.id('btnSubmit')).click();
+    browser.driver.sleep(1000);
+    expect(element(by.id('phoneError')).isPresent()).toBe(true);
+    element(by.id('phone')).sendKeys('74852186');
+    element(by.id('btnSubmit')).click();
+    expect(element(by.id('phoneError')).isPresent()).toBe(true);
+  });
+
+  it('Register - CGU not checked', () => {
+    element(by.id('cgu')).click();
+    browser.driver.sleep(1000);
+    expect(element(by.id('cguError')).isPresent()).toBe(true);
+  });
+
+
+
 });
