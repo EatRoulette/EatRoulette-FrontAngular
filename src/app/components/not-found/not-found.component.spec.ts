18b8ff91793cd5905fce768f5cfe6106cf920b8f
Aymeric Larvet <aymeric.larvet@habiteo.com>
2020-07-09 12:18:05 +0200

    :white_check_mark: fix bad url test

--- src/app/components/not-found/not-found.component.spec.ts
+++ src/app/components/not-found/not-found.component.spec.ts
@@ -4,25 +4,6 @@ import { NotFoundComponent } from './not-found.component';
 import {browser} from "protractor";
 
 describe('NotFoundComponent', () => {
-  let component: NotFoundComponent;
-  let fixture: ComponentFixture<NotFoundComponent>;
-
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ NotFoundComponent ]
-  //   })
-  //   .compileComponents();
-  // }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NotFoundComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 
   it('Bad url redirect to 404 not found', () => {
     browser.get('http://localhost:4200/NotAGoodURL');
@@ -30,7 +11,5 @@ describe('NotFoundComponent', () => {
     const expectedUrl = browser.driver.getCurrentUrl();
     expect(expectedUrl).toEqual('http://localhost:4200/404');
   });
-
-
 
 });
