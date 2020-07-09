4c41dd53ee52aec850d353c2827398aa7b3b5067
Gwen <gwendou95@hotmail.fr>
2020-07-09 11:18:01 +0200

    :white_check_mark: Add test on 404 page

--- src/app/components/not-found/not-found.component.spec.ts
+++ src/app/components/not-found/not-found.component.spec.ts
@@ -1,17 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotFoundComponent } from './not-found.component';
+import {browser} from "protractor";
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
   let fixture: ComponentFixture<NotFoundComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NotFoundComponent ]
-    })
-    .compileComponents();
-  }));
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [ NotFoundComponent ]
+  //   })
+  //   .compileComponents();
+  // }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NotFoundComponent);
@@ -22,4 +23,14 @@ describe('NotFoundComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Bad url redirect to 404 not found', () => {
+    browser.get('http://localhost:4200/NotAGoodURL');
+    browser.driver.sleep(3000);
+    const expectedUrl = browser.driver.getCurrentUrl();
+    expect(expectedUrl).toEqual('http://localhost:4200/404');
+  });
+
+
+
 });
