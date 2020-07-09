3e9137be991d6b6f2be23ef4a16a51380f6943b5
Gwen <gwendou95@hotmail.fr>
2020-07-09 19:30:12 +0200

    :sparkles: Add routing for restaurant detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,19 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {RegisterComponent} from './components/register/register.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {LoginComponent} from "./components/login/login.component";
-import {NotFoundComponent} from "./components/not-found/not-found.component";
-import {DashboardComponent} from "./components/dashboard/dashboard.component";
-import {SituationComponent} from "./components/situation/situation.component";
-import {AuthGuard} from "./auth/auth.guard";
-import {SupportComponent} from "./components/support/support.component";
-import {TicketsComponent} from "./components/tickets/tickets.component";
-import {TicketDetailsComponent} from "./components/ticket-details/ticket-details.component";
-import {SearchComponent} from "./components/search/search.component";
-import {AddRestaurantComponent} from "./components/add-restaurant/add-restaurant.component";
-import {FriendsComponent} from "./components/friends/friends.component";
-import {MyListsComponent} from "./components/my-lists/my-lists.component";
+import {LoginComponent} from './components/login/login.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {SituationComponent} from './components/situation/situation.component';
+import {AuthGuard} from './auth/auth.guard';
+import {SupportComponent} from './components/support/support.component';
+import {TicketsComponent} from './components/tickets/tickets.component';
+import {TicketDetailsComponent} from './components/ticket-details/ticket-details.component';
+import {SearchComponent} from './components/search/search.component';
+import {AddRestaurantComponent} from './components/add-restaurant/add-restaurant.component';
+import {FriendsComponent} from './components/friends/friends.component';
+import {MyListsComponent} from './components/my-lists/my-lists.component';
+import {RestaurantDetailComponent} from './components/restaurant-detail/restaurant-detail.component';
 
 const routes: Routes = [
   {
@@ -70,6 +71,13 @@ const routes: Routes = [
     component: AddRestaurantComponent,
     data: {
       title: 'Page d\'ajout de restaurant'
+    }
+  },
+  {
+    path: 'restaurant/:idRestaurant',
+    component: RestaurantDetailComponent,
+    data: {
+      title: 'Restaurant'
     }
   },
   {
