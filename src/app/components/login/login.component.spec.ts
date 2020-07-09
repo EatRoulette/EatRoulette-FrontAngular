d15eaedb0d6ee119e6eda15b5fc8f7831869a8a9
Aymeric Larvet <aymeric.larvet@habiteo.com>
2020-07-09 12:13:39 +0200

    :white_check_mark: fix user not logged test

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -9,28 +9,6 @@ describe('LoginComponent', () => {
   // tslint:disable-next-line:prefer-const
   let fixture: ComponentFixture<LoginComponent>;
 
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ LoginComponent ]
-  //   })
-  //   .compileComponents();
-  // }));
-  //
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(LoginComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('Test of the test', () => {
-
-  });
-
-
   // tslint:disable-next-line:only-arrow-functions
   it('login test', function() {
     browser.get('http://localhost:4200/login');
@@ -40,7 +18,7 @@ describe('LoginComponent', () => {
 
     const value = browser.executeScript('return window.localStorage.getItem(\'access_token\');');
     expect(value).not.toBe('');
-    browser.driver.sleep(3000);
+    browser.driver.sleep(1000);
     // tslint:disable-next-line:prefer-const
     let expectedUrl = browser.driver.getCurrentUrl();
     expect(expectedUrl).toEqual('http://localhost:4200/');
@@ -48,8 +26,9 @@ describe('LoginComponent', () => {
 
   // tslint:disable-next-line:only-arrow-functions
   it('User not logged', () => {
+      browser.executeScript('window.localStorage.removeItem(\'access_token\');');
       browser.get('http://localhost:4200/tickets');
-      browser.driver.sleep(3000);
+      browser.driver.sleep(1000);
       const expectedUrl = browser.driver.getCurrentUrl();
       expect(expectedUrl).toEqual('http://localhost:4200/login');
   });
