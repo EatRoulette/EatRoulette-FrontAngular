0e30595e3fdc2c3b8abfe70471a9e6eae1a2b9a8
Aymeric Larvet <aymeric.larvet@habiteo.com>
2020-07-09 10:02:43 +0200

    :white_check_mark: add login test

--- src/app/components/friends/friends.component.spec.ts
+++ src/app/components/friends/friends.component.spec.ts
@@ -1,25 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FriendsComponent } from './friends.component';
+import {browser, by, element} from "protractor";
 
 describe('FriendsComponent', () => {
   let component: FriendsComponent;
   let fixture: ComponentFixture<FriendsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FriendsComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
-    fixture = TestBed.createComponent(FriendsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    console.log('COUCOU');
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create new friendlist', function() {
+    browser.get('http://localhost:4200/friends');
+
+    element(by.id('test')).click();
+    element(by.id('name')).sendKeys('test friendlist');
+    element(by.id('add')).click();
+
+    const todoList = element.all(by.binding('groupSelection.name'));
+    expect(todoList.count()).toEqual(1);
+    expect(todoList.get(0).getText()).toEqual('test friendlist');
+
   });
 });
--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,25 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {browser, by, element, protractor} from "protractor";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [ LoginComponent ]
+  //   })
+  //   .compileComponents();
+  // }));
+  //
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(LoginComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('login test', function() {
+    browser.get('http://localhost:4200/login');
+    element(by.id('email')).sendKeys('testu@gmail.com');
+    element(by.id('password')).sendKeys('testu');
+    element(by.id('login')).click();
+
+    const value = browser.executeScript("return window.localStorage.getItem('access_token');");
+    expect(value).not.toBe('');
+    browser.driver.sleep(3000);
+    var expectedUrl = browser.driver.getCurrentUrl();
+    expect(expectedUrl).toEqual('http://localhost:4200/');
+  });
+
+  it('user not login', function() {
+      browser.get('http://localhost:4200/  ');
+  })
 });
