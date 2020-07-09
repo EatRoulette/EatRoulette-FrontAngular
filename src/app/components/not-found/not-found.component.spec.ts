956703463777715041b7af8ac22b65fa6a394b32
Gwen <gwendou95@hotmail.fr>
2020-07-09 14:50:22 +0200

    ✅ Add test on 404 page

--- src/app/components/not-found/not-found.component.spec.ts
+++ src/app/components/not-found/not-found.component.spec.ts
@@ -1,15 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotFoundComponent } from './not-found.component';
-import {browser} from "protractor";
+import {browser, by, element} from 'protractor';
 
 describe('NotFoundComponent', () => {
 
   it('Bad url redirect to 404 not found', () => {
     browser.get('http://localhost:4200/NotAGoodURL');
-    browser.driver.sleep(3000);
+    browser.driver.sleep(1000);
     const expectedUrl = browser.driver.getCurrentUrl();
     expect(expectedUrl).toEqual('http://localhost:4200/404');
   });
 
+  it('Not found page redirect to home', () => {
+    browser.get('http://localhost:4200/404');
+    element(by.id('btnHome')).click();
+    browser.driver.sleep(1000);
+    const expectedUrl = browser.driver.getCurrentUrl();
+    expect(expectedUrl).toEqual('http://localhost:4200/');
+  });
+
+
+
 });
+
