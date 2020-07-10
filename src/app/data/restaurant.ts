c19887baf1836e7f0cf7c3a929a02a4a951025dd
Gwen <gwendou95@hotmail.fr>
2020-07-10 19:09:57 +0200

    ✅ Restaurant unit testing

--- src/app/data/restaurant.ts
+++ src/app/data/restaurant.ts
@@ -1,5 +1,5 @@
-import {Characteristic} from "./characteristic";
-import {Allergen} from "./allergen";
+import {Characteristic} from './characteristic';
+import {Allergen} from './allergen';
 
 export class Restaurant {
   id: string;
@@ -9,6 +9,23 @@ export class Restaurant {
   city: string;
   postalCode: string;
   characteristics: Characteristic[];
-  allergens : Allergen[];
+  allergens: Allergen[];
   website: string;
+
+  constructor(id: string, name: string,
+              type: string, address: string,
+              city: string, postalCode: string,
+              characteristics: Characteristic[], allergens: Allergen[], website: string) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.address = address;
+    this.city = city;
+    this.postalCode = postalCode;
+    this.characteristics = characteristics;
+    this.allergens = allergens;
+    this.website = website;
+  }
+
+
 }
